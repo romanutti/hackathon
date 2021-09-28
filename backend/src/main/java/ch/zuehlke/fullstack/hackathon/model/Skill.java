b7fa53319d633addacb035ab07ecff0c9ae3aeda
Ziltener, Stefanie <stefanie.ziltener@zuehlke.com>
2021-09-28 17:31:59 +0200

    Add logic to skill points

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Skill.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/model/Skill.java
@@ -1,6 +1,7 @@
 package ch.zuehlke.fullstack.hackathon.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -12,15 +13,19 @@ public class Skill {
     // todo: find out how to map this
     private final long pictureId = 1;
     // todo: calculate rank via trainings
+    private final Level level;
     private int rank;
+    private long points = 0;
 
     @JsonCreator
     public Skill(@JsonProperty("Id") long skillId,
                  @JsonProperty("ShortDescription") String description,
-                 @JsonProperty("Name") String name) {
+                 @JsonProperty("Name") String name,
+                 @JsonProperty("ExperienceText") Level level) {
         this.skillId = skillId;
         this.description = description;
         this.name = name;
+        this.level = level;
     }
 
     @JsonProperty("skillId")
@@ -50,5 +55,19 @@ public class Skill {
 
     public void setRank(int rank) {
         this.rank = rank;
+    }
+
+    @JsonIgnore
+    public long getPoints() {
+        return points;
+    }
+
+    @JsonIgnore
+    public void addPoints(long points) {
+        this.points += points;
+    }
+    
+    public Level getLevel() {
+        return level;
     }
 }
