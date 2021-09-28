5f54aba2f5540bb31e47dcdab38ec4b43f6cea40
LewinGerber <57451950+LewinGerber@users.noreply.github.com>
2021-09-28 14:49:05 +0200

    Feature/search (#7)
    * Add search component

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {UserInfoDto} from "../../model/UserInfoDto";
+import {HttpErrorResponse} from "@angular/common/http";
+import {AppService} from "../app.service";
+import {FormControl, FormGroup} from "@angular/forms";
+import {debounceTime} from "rxjs/operators";
+import {SearchError} from "../../model/search/SearchError";
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  onInputChange$: Subscription | undefined;
+  httpError: SearchError = {};
+  debounceMilliseconds: number = 300;
 
-  constructor() { }
+  search: FormGroup = new FormGroup({
+    searchTerm: new FormControl()
+  });
 
-  ngOnInit(): void {
+  @Output()
+  foundUser = new EventEmitter<string>();
+
+  constructor(private readonly appService: AppService) { }
+
+  ngOnInit() {
+    this.onInputChange$ = this.search.valueChanges
+      .pipe(debounceTime(this.debounceMilliseconds))
+      .subscribe(data =>
+        this.searchForTerm(data.searchTerm));
   }
 
+  ngOnDestroy() {
+    if(this.onInputChange$) {
+      this.onInputChange$.unsubscribe();
+    }
+  }
+
+  searchForTerm(searchTerm:string): void {
+    this.appService.getUserInfo(searchTerm)
+      .subscribe({
+        next: (userInfo: UserInfoDto) => {
+          this.foundUser.emit(userInfo.id);
+        },
+        error: (error: HttpErrorResponse) => {
+          this.httpError.searchError = error.status;
+        },
+      })
+  }
 }
