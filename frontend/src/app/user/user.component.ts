e51514909eb47e77682448822b2d929da1c47dec
Marco Romanutti <m.romanutti@gmail.com>
2021-09-29 10:24:48 +0200

    Fix wrong user lookups

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -18,10 +18,8 @@ import { AppService } from '../app.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit, OnChanges {
+export class UserComponent implements OnChanges {
   @Input()
-  userId: string = '';
-
   userInfo: UserInfoDto | undefined;
   badges: BadgeDto[] | undefined;
   skills: SkillDto[] | undefined;
@@ -32,53 +30,55 @@ export class UserComponent implements OnInit, OnChanges {
     private modalService: NgbModal
   ) {}
 
-  ngOnInit(): void {
-    this.loadUserInfo();
-    this.loadBadges();
-    this.loadSkills();
-  }
-
   ngOnChanges(changes: SimpleChanges) {
-    this.userId = changes.userId.currentValue;
-    this.loadUserInfo();
-    this.loadBadges();
-    this.loadSkills();
+    if (this.userInfo) {
+      this.userInfo = changes.userInfo.currentValue;
+      this.loadUserInfo();
+      this.loadBadges();
+      this.loadSkills();
+    }
   }
 
   loadUserInfo() {
-    this.appService.getUserInfo(this.userId).subscribe({
-      next: (userInfo) => {
-        this.userInfo = userInfo;
-        this.httpError.userInfoError = undefined;
-      },
-      error: (error: HttpErrorResponse) => {
-        this.httpError.userInfoError = error.status;
-      },
-    });
+    if (this.userInfo) {
+      this.appService.getUserInfo(this.userInfo.id).subscribe({
+        next: (userInfo) => {
+          this.userInfo = userInfo;
+          this.httpError.userInfoError = undefined;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.httpError.userInfoError = error.status;
+        },
+      });
+    }
   }
 
   loadBadges() {
-    this.appService.getBadges(this.userId).subscribe({
-      next: (badges) => {
-        this.badges = badges;
-        this.httpError.badgesError = undefined;
-      },
-      error: (error: HttpErrorResponse) => {
-        this.httpError.badgesError = error.status;
-      },
-    });
+    if (this.userInfo) {
+      this.appService.getBadges(this.userInfo.id).subscribe({
+        next: (badges) => {
+          this.badges = badges;
+          this.httpError.badgesError = undefined;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.httpError.badgesError = error.status;
+        },
+      });
+    }
   }
 
   loadSkills() {
-    this.appService.getSkills(this.userId).subscribe({
-      next: (skills) => {
-        this.skills = skills;
-        this.httpError.skillsError = undefined;
-      },
-      error: (error: HttpErrorResponse) => {
-        this.httpError.skillsError = error.status;
-      },
-    });
+    if (this.userInfo) {
+      this.appService.getSkills(this.userInfo.id).subscribe({
+        next: (skills) => {
+          this.skills = skills;
+          this.httpError.skillsError = undefined;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.httpError.skillsError = error.status;
+        },
+      });
+    }
   }
 
   open(content: any) {
